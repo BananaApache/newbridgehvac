4e1b01513f9e973daf71ee757ff774097e19c193
daniel <daniel@the-chink-machine.local>
2023-05-20 20:35:34 -0400

    Added new views method

--- newb_settings/urls.py
+++ newb_settings/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.send_to_home),
     path("home/", views.index)
 ]
--- newb_settings/views.py
+++ newb_settings/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def send_to_home(request):
+    return redirect(index)
